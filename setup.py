4f8165fa3ae2bbe72b21f49156598387ee18b94a
Jonathan Helmus <jjhelmus@gmail.com>
2019-05-05 15:06:07 -0400

    Add long_description, 0.0.2

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='break_my_python',
-    version='0.0.1',
+    version='0.0.2',
     description='This package tries to breaks your python interpreter, do not install it',
-    long_description='',
+    long_description=long_description,
     author='Jonathan J. Helmus',
     author_email='jjhelmus@gmail.com',
     url='http://pypi.python.org/pypi/break_my_python/',
